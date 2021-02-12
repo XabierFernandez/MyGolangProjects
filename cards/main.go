6b5520af17ea86c613db20331177f8b633d23fc7
Xabier Fernandez <xabier.fernandez@outlook.com>
2021-02-12 17:48:48 +0100

    cards package

--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	//var card string = "Ace of Spades"
-	cards := []string{newCard(), newCard()}
-	cards = append(cards, newCard())
-	fmt.Println(cards)
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.print()
 }
